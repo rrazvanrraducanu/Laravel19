e4a3a56365a49d46fc0f3c95634f363802b79d82
Razvan Raducanu <rrazvan.rraducanu@gmail.com>
2018-03-26 08:25:39 +0300

    Validation

--- app/Http/Controllers/FlowersController.php
+++ app/Http/Controllers/FlowersController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 //use Illuminate\Http\Request;
+use Validator;
 use Request;
 use App\Flower;
+
 
 class FlowersController extends Controller
 {
@@ -27,6 +29,15 @@ class FlowersController extends Controller
 
     public function store()
     {
+        $validation = Validator::make(Request::all(), [
+            'nume' => 'required',
+            'culoare' => 'required',
+            'marime' => 'required',
+            'pret' => 'required'
+        ]);
+        if ($validation->fails()) {
+            return redirect()->back()->withInput()->withErrors($validation->messages());
+        }
         $flower = new Flower;
         $flower->nume = Request::input('nume');
         $flower->marime = Request::input('marime');
--- resources/views/pages/flowers/addflower.blade.php
+++ resources/views/pages/flowers/addflower.blade.php
@@ -1,16 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head><title>Add a flower</title></head>
 <body>
 <?php
-echo Form::open(array('action'=>'FlowersController@store')) ;
+echo Form::open(array('action' => 'FlowersController@store')) ;
 echo Form::label('nume', 'Nume: ');
-echo Form::text('nume')."<br><br>";
+echo Form::text('nume');
+echo $errors->first('nume')."<br/><br/>";
+
 echo Form::label('marime', 'Marime: ');
-echo Form::text('marime')."<br><br>";
+echo Form::text('marime');
+echo $errors->first('marime')."<br/><br/>";
+
 echo Form::label('culoare', 'Culoare: ');
-echo Form::text('culoare')."<br><br>";
+echo Form::text('culoare');
+echo $errors->first('culoare')."<br/><br/>";
+
 echo Form::label('pret', 'Pret: ');
-echo Form::text('pret')."<br><br>";
+echo Form::text('pret');
+echo $errors->first('pret')."<br/><br/>";
+
 echo Form::submit('Add flower');
 echo Form::close();
 ?>
